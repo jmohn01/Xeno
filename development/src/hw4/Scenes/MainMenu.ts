ea50980621d784d4a69f6a6af17a914629101dde
Cheese-S <yzcalex@icloud.com>
2022-04-08 00:02:15 -0400

    Menus

--- development/src/hw4/Scenes/MainMenu.ts
+++ development/src/hw4/Scenes/MainMenu.ts
@@ -77,6 +77,42 @@ export default class MainMenu extends Scene {
         helpBtn.onClickEventId = "help"; 
 
         /* ------------------------------ CHAPTER LAYER ----------------------------- */
+        const chapterTitleLine = <Label>this.add.uiElement(UIElementType.LABEL, "chapter", { position: new Vec2(titlePosition.x, titlePosition.y), text: "CHAPTERS" })
+        chapterTitleLine.textColor = Color.BLACK;
+        chapterTitleLine.fontSize = 78;
+
+        const chapter1_1Btn = this.add.uiElement(UIElementType.BUTTON, "chapter", { position: new Vec2(CANVAS_SIZE.x * 0.25, 4 * PADDING), text: '1-1' })
+        chapter1_1Btn.borderColor = chapter1_1Btn.backgroundColor = PassiveGrey;
+        chapter1_1Btn.size.set(100, 100);
+        chapter1_1Btn.onClickEventId = "chapter1_1"; 
+        const chapter1_2Btn = this.add.uiElement(UIElementType.BUTTON, "chapter", { position: new Vec2(CANVAS_SIZE.x * 0.25 , 6 * PADDING), text: '1-2' })
+        chapter1_2Btn.borderColor = chapter1_2Btn.backgroundColor = PassiveGrey;
+        chapter1_2Btn.size.set(100, 100);
+        chapter1_2Btn.onClickEventId = "chapter1_2";
+        const chapter2_1Btn = this.add.uiElement(UIElementType.BUTTON, "chapter", { position: new Vec2(CANVAS_SIZE.x * 0.5, 4 * PADDING), text: '2-1' })
+        chapter2_1Btn.borderColor = chapter2_1Btn.backgroundColor = PassiveGrey;
+        chapter2_1Btn.size.set(100, 100);
+        chapter2_1Btn.onClickEventId = "chapter2_1";
+        const chapter2_2Btn = this.add.uiElement(UIElementType.BUTTON, "chapter", { position: new Vec2(CANVAS_SIZE.x * 0.5, 6 * PADDING), text: '2-2' })
+        chapter2_2Btn.borderColor = chapter2_2Btn.backgroundColor = PassiveGrey;
+        chapter2_2Btn.size.set(100, 100);
+        chapter2_2Btn.onClickEventId = "chapter2_2";
+        const chapter3_1Btn = this.add.uiElement(UIElementType.BUTTON, "chapter", { position: new Vec2(CANVAS_SIZE.x * 0.75, 4 * PADDING), text: '3-1' })
+        chapter3_1Btn.borderColor = chapter3_1Btn.backgroundColor = PassiveGrey;
+        chapter3_1Btn.size.set(100, 100);
+        chapter3_1Btn.onClickEventId = "chapter3_1";
+        const chapter3_2Btn = this.add.uiElement(UIElementType.BUTTON, "chapter", { position: new Vec2(CANVAS_SIZE.x * 0.75 , 6 * PADDING), text: '3-2' })
+        chapter3_2Btn.borderColor = chapter3_2Btn.backgroundColor = PassiveGrey;
+        chapter3_2Btn.size.set(100, 100);
+        chapter3_2Btn.onClickEventId = "chapter3_2";
+
+
+        const chapterBackBtn = this.add.uiElement(UIElementType.BUTTON, "chapter", { position: new Vec2(CANVAS_SIZE.x * 0.9, 8 * PADDING), text: 'BACK' });
+        chapterBackBtn.borderColor = chapterBackBtn.backgroundColor = ActiveGrey; 
+        chapterBackBtn.size.set(200, 50);
+        chapterBackBtn.onClickEventId = "main";
+
+
 
         /* ------------------------------ CONTROL LAYER ----------------------------- */
         const controlTitleLine = <Label>this.add.uiElement(UIElementType.LABEL, "control", { position: new Vec2(titlePosition.x, titlePosition.y), text: "COTNROL" })
@@ -114,22 +150,21 @@ export default class MainMenu extends Scene {
         helpTitleLine.textColor = Color.BLACK;
         helpTitleLine.fontSize = 78;
         
-        const helpDeveLine = <Label>this.add.uiElement(UIElementType.LABEL, "help", { postion: new Vec2(center.x, 2 * PADDING), text: "This game is developed by Chencheng Yang, Hongcheng Li, and xxx"});
+        const helpDeveLine = <Label>this.add.uiElement(UIElementType.LABEL, "help", { position: new Vec2(center.x, 3 * PADDING), text: "Xeno is developed by Chencheng Yang, Hongcheng Li, and XXX" })
 
-        const helpStoryLine1 = <Label>this.add.uiElement(UIElementType.LABEL, "help", { postion: new Vec2(center.x, 3 * PADDING), text: "Your are part of the intergalactic explorers, \"Xeno\"."});
-        const helpStoryLine2 = <Label>this.add.uiElement(UIElementType.LABEL, "help", { postion: new Vec2(center.x, 4 * PADDING), text: "Utilize your resource to build up a fortress and defend against the vicious UMAs"});
-        const helpStoryLine3 = <Label>this.add.uiElement(UIElementType.LABEL, "help", { postion: new Vec2(center.x, 5 * PADDING), text: "Don't let your guard down or you will be eaten alive "});
+        const helpStoryLine1 = <Label>this.add.uiElement(UIElementType.LABEL, "help", { position: new Vec2(center.x, 4 * PADDING), text: "Your are part of the intergalactic explorers, \"Xeno\""});
+        const helpStoryLine2 = <Label>this.add.uiElement(UIElementType.LABEL, "help", { position: new Vec2(center.x, 4.5 * PADDING), text: "Utilize your resource to build up a fortress and defend against the vicious UMAs"});
+        const helpStoryLine3 = <Label>this.add.uiElement(UIElementType.LABEL, "help", { position: new Vec2(center.x, 5 * PADDING), text: "Don't let your guard down or you will be eaten alive "});
 
         const semicolon = this.add.sprite("semicolon", "help");
         semicolon.position = new Vec2(titlePosition.x, 6 * PADDING);
-        const semicolonLine = <Label>this.add.uiElement(UIElementType.LABEL, "control", { position: new Vec2(CANVAS_SIZE.x * 0.6, 6 * PADDING), text: 'Infinite Money' });
+        const semicolonLine = <Label>this.add.uiElement(UIElementType.LABEL, "help", { position: new Vec2(CANVAS_SIZE.x * 0.6, 6 * PADDING), text: 'Infinite Money' });
         semicolonLine.backgroundColor = semicolonLine.borderColor = Color.TRANSPARENT;
         semicolonLine.fontSize = 40;
         semicolonLine.textColor = Color.BLACK; 
-
         const quote = this.add.sprite("quote", "help");
         quote.position = new Vec2(titlePosition.x, 7 * PADDING);
-        const quoteLine = <Label>this.add.uiElement(UIElementType.LABEL, "control", { position: new Vec2(CANVAS_SIZE.x * 0.6, 7 * PADDING), text: 'Infinite Health' });
+        const quoteLine = <Label>this.add.uiElement(UIElementType.LABEL, "help", { position: new Vec2(CANVAS_SIZE.x * 0.6, 7 * PADDING), text: 'Infinite Health' });
         quoteLine.backgroundColor = quoteLine.borderColor = Color.TRANSPARENT;
         quoteLine.fontSize = 40;
         quoteLine.textColor = Color.BLACK; 
