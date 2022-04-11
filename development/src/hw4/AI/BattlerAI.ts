474eac76f6ec8ad89ff290634acd11d0d8bd372a
Cheese-S <yzcalex@icloud.com>
2022-04-11 16:20:19 -0400

    stop for now

--- development/src/hw4/AI/BattlerAI.ts
+++ development/src/hw4/AI/BattlerAI.ts
@@ -1,7 +1,9 @@
 import AI from "../../Wolfie2D/DataTypes/Interfaces/AI";
 import GameEvent from "../../Wolfie2D/Events/GameEvent";
 import GameNode from "../../Wolfie2D/Nodes/GameNode";
+import AOEAttack from "../GameSystems/Attack/AOEAttack";
 import { EffectData } from "../GameSystems/Attack/internal";
+import PointAttack from "../GameSystems/Attack/PointAttack";
 import { Effect } from "../GameSystems/Effect/Effect";
 
 export default interface BattlerAI extends AI {
@@ -16,6 +18,8 @@ export default interface BattlerAI extends AI {
     effects: Array<Effect<any>>
 
     atkEffect: EffectData
+
+    atk: PointAttack | AOEAttack
 
     damage(damage: number): void;
 
